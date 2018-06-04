e12addc6d63ba1ab9cc286e802425294370296f1
Bigghead <shavenchew@yahoo.com>
2018-06-04 12:41:28 -0700

    reusable player animation function

--- src/scenes/Level_1.js
+++ src/scenes/Level_1.js
@@ -62,9 +62,9 @@ export class Level_1 extends Scene {
     // ===== If space key is held, add new logic to move direction ===== //
     if( this.SPACE_KEY.isDown ) {
       if( this.cursors.left.isDown ) {
-          this.playerRunAnimate(this.player, { setVelocityX: -160, flipX: false } );
+          this.playerAnimate(this.player, { animation: 'run', setVelocityX: -160, flipX: false } );
       } else if( this.cursors.right.isDown ) {
-          this.playerRunAnimate(this.player, { setVelocityX: 160, flipX: true } );
+          this.playerAnimate(this.player, { animation:'run', setVelocityX: 160, flipX: true } );
       }
     }
 
@@ -73,7 +73,6 @@ export class Level_1 extends Scene {
 
       this.player.setVelocityX(-160);
       this.player.flipX = false;
-
       // ===== Check to see if we should swing sword ===== //
       if (this.Z_KEY.isDown) {
 
@@ -86,8 +85,7 @@ export class Level_1 extends Scene {
         // ===== Check if player is currently sliding. ===== //
         // ===== Do slide, stop animation after 3/10ths of a second ===== //
         if (!this.isSliding) {
-          this.player.anims.play('slide');
-          this.player.setVelocityX(-250);
+          this.playerAnimate(this.player, { animation: 'slide', setVelocityX: -250 } );
           this.time.delayedCall(200, () => this.isSliding = true)
         }
 
@@ -104,17 +102,17 @@ export class Level_1 extends Scene {
     // ===== Need to flip frames on x axis. Sprites dont have moving to the right frames ===== //
     // ===== All same logic as above, but backwards ===== //
     else if (this.cursors.right.isDown) {
-
+      
       this.player.setVelocityX(160);
       this.player.flipX = true;
       if (this.Z_KEY.isDown) {
 
         this.player.anims.play('sword', true);
+
       } else if (this.SPACE_KEY.isDown) {
 
         if (!this.isSliding) {
-          this.player.anims.play('slide');
-          this.player.setVelocityX(250);
+          this.playerAnimate(this.player, { animation: 'slide', setVelocityX: 250, flipX: true } );
           this.time.delayedCall(200, () => this.isSliding = true)
         }
 
@@ -142,9 +140,9 @@ export class Level_1 extends Scene {
    * @param {player object} player 
    * @param {player animation options object} [{ setVelocityX = 0 , flipX = false }={}] 
    */
-  playerRunAnimate(player, { setVelocityX = 0 , flipX = false } = {} ) {
+  playerAnimate(player, { animation = 'run', setVelocityX = 0 , flipX = false } = {} ) {
     player.setVelocityX(setVelocityX);
     player.flipX = flipX;
-    player.anims.play('run', true);
+    player.anims.play(animation, true);
   }
 }
