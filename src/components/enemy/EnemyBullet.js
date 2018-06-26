29f529d61319e019e02941adc05bf6d6571b4cb0
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-06-26 22:07:18 +0200

    Aim bullet towards player  when fired

--- src/components/enemy/EnemyBullet.js
+++ src/components/enemy/EnemyBullet.js
@@ -3,21 +3,42 @@ import { HEIGHT } from "../../util/constants";
 export default class EnemyBullet extends Phaser.GameObjects.Image {
   constructor(config) {
     super(config, 0, 0, "enemy-bullet");
-    this.speed = Phaser.Math.GetSpeed(200, 1);
+    this.speedX = 0;
+    this.speedY = 0;
+    this.targetX = 0;
+    this.startingX = 0;
+    this.startingY = 0;
   }
 
-  fire(x, y) {
-    this.setPosition(x, y);
+  fire(startingX, startingY, targetX, targetY) {
+    this.setPosition(startingX, startingY);
+    this.targetX = targetX;
+    this.startingX = startingX;
+    this.startingY = startingY;
+    const distanceY = HEIGHT - startingY;
+    const distanceX = this.targetX - this.startingX;
+    this.speedY = Phaser.Math.GetSpeed(distanceY, 1);
+    this.speedX = Phaser.Math.GetSpeed(distanceX, 1);
+
+    let targetAngle = Phaser.Math.Angle.Between(
+      this.startingX,
+      this.startingY,
+      this.targetX,
+      HEIGHT
+    );
+    targetAngle = Phaser.Math.RadToDeg(targetAngle);
+    targetAngle = this.targetX > this.startingX ? -targetAngle : targetAngle;
+
+    this.angle = targetAngle;
 
     this.setActive(true);
     this.setVisible(true);
   }
 
   update(time, delta) {
-    this.y += this.speed * 30;
+    this.y += this.speedY * 30;
+    this.x += this.speedX * 30;
 
-    // comment below line in if you want to see the bullet children get deleted
-    // console.log(this.scene.bullets.children.entries.length)
     if (this.y > HEIGHT) {
       this.setActive(false);
       this.setVisible(false);
