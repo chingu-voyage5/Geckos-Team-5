810e0798965210241456f680c6a2f17c93d6a8c9
fancyaction <labcoatdisaster@gmail.com>
2018-06-28 18:36:44 -0500

    Add how to play scene

--- src/scenes/Help.js
+++ src/scenes/Help.js
@@ -7,4 +7,39 @@ export class Help extends Scene {
       key: 'Help'
     });
   }
+
+  create() {
+    // const text = this.add.bitmapText(0, 0, UIFONT, this.text(), 8);
+    const text = this.add
+      .text(WIDTH / 2, HEIGHT - 160, this.text(), UIFONT)
+      .setOrigin(0.5)
+      .setScale(1);
+
+    this.keys = {
+      space: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE),
+      esc: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC)
+    };
+  }
+
+  text() {
+    return `\n
+    CONTROLS\n\n
+    ARROW KEYS: MOVE\n
+    A: SWORD ATTACK \n
+    S: THROW CARD\n
+    C: BOMB \n
+    SPACE: SLIDE\n
+    -OR USE JOYPADS- \n
+    [esc] to title
+    `;
+  }
+
+  update() {
+    if (
+      Phaser.Input.Keyboard.JustDown(this.keys.esc) ||
+      Phaser.Input.Keyboard.JustDown(this.keys.space)
+    ) {
+      this.scene.start('Title');
+    }
+  }
 }
