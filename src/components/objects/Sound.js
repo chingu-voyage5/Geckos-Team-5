3238c75ae66507a1179826b44943fb8957bf6aa6
alexever17@gmail.com <alexever17@gmail.com>
2018-07-07 20:53:10 +0200

    closes #165 and adds new brick and sword sound

--- src/components/objects/Sound.js
+++ src/components/objects/Sound.js
@@ -9,6 +9,15 @@ function soundAdder(scene) {
     scene.sound_gamewin = scene.sound.add('gamewin');
     scene.sound_sword = scene.sound.add('sword');
     scene.sound_life = scene.sound.add('life');
+
+    scene.sound_gameover.config.volume = 2;
+    scene.sound_brick.config.volume = 2;
+    scene.sound_ballplayer.config.volume = 10;
+    scene.sound_ballwalls.config.volume = 50;
+    scene.sound_bullet.config.volume = 0.8;
+    scene.sound_gamewin.config.volume = 1;
+    scene.sound_sword.config.volume = 1;
+    scene.sound_life.config.volume = 4;
 }
 
 //reduces the need to make an if statement everytime
