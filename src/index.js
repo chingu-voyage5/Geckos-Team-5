f8f2575ee470caa1352d296ac382ebf4175a7c69
alexever17@gmail.com <alexever17@gmail.com>
2018-07-21 20:29:48 +0200

    commenting source

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import HeartPlugin from './components/objects/HeartPlugin';
 
 const config = {
   type: Phaser.AUTO,
-  width: WIDTH,
+  width: WIDTH, // height and width are loaded from the constants file
   height: HEIGHT,
   input: {
     gamepad: true
@@ -18,7 +18,7 @@ const config = {
   pixelArt: true,
   parent: 'phaser',
   plugins: {
-    global: [{ key: 'HeartPlugin', plugin: HeartPlugin, start: true }]
+    global: [{ key: 'HeartPlugin', plugin: HeartPlugin, start: true }] //is used to display the life amount of the player
   },
   physics: {
     default: 'arcade',
@@ -27,10 +27,7 @@ const config = {
       debug: false
     }
   },
-  scene: [Preloader, Title, Credits, Help, Level_1, UIScene],
-  audio: {
-    // disableWebAudio: true
-  }
+  scene: [Preloader, Title, Credits, Help, Level_1, UIScene]
 };
 
 const game = new Phaser.Game(config);
